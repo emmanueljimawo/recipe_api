56226185046da8ef1758a1addec24e79663cee92
emmanuel <emmanueljimawo@yahoo.co.uk>
2019-06-12 15:22:25 +0100

    added code

--- recipe_app/app/tests/test_admin.py
+++ recipe_app/app/tests/test_admin.py
@@ -4,5 +4,30 @@ from django.urls import reverse
 
 class AdminSiteTests(TestCase):
 
-    def set_up(self):
-        
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@xyz.com',
+            password = 'password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'test@xyz.com',
+            password = 'test123',
+            name = 'Test User full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:app_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:app_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
