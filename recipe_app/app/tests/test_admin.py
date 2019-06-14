c5a661095257f719f356af738334bf5fdf9345a7
emmanuel <emmanueljimawo@yahoo.co.uk>
2019-06-14 16:50:59 +0100

    admin test

--- recipe_app/app/tests/test_admin.py
+++ recipe_app/app/tests/test_admin.py
@@ -34,7 +34,7 @@ class AdminSiteTests(TestCase):
 
     def test_create_user_page(self):
         """Test create user page works"""
-        url = reverse('admin:core_user_add')
+        url = reverse('admin:app_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
